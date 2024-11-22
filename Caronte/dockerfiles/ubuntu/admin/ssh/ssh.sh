3d1a3a2ae845d305c1924d5c785345c65229833d
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-11-22 12:53:12 +0100

    config ssh

---
+++ Caronte/dockerfiles/ubuntu/admin/ssh/ssh.sh
@@ -0,0 +1,10 @@
+config_ssh(){
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+    if [ ! -d /home/${USUARIO}/.ssh ]
+    then
+        mkdir /home/${USUARIO}/.ssh
+        cat /root/datos/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    fi
+    /etc/init.d/ssh start
+}
--- Caronte/dockerfiles/ubuntu/admin/start.sh
+++ Caronte/dockerfiles/ubuntu/admin/start.sh
@@ -2,11 +2,14 @@
 set -e
 
 source ./usuarios/usuarios.sh
+source ./ssh/ssh.sh
 
 chmod +x /root/admin/usuarios/usuarios.sh
+chmod +x /root/admin/ssh/ssh.sh
 
 main(){
    newUser  
+   config_ssh
    # make_bienvenida
 
     tail -f /dev/null 
