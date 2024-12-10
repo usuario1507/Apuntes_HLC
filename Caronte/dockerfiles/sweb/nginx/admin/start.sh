321e98de551fabf63da3263d74e32d325cfeb001
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-12-10 08:44:24 +0100

    nginx/base

--- Caronte/dockerfiles/sweb/nginx/admin/start.sh
+++ Caronte/dockerfiles/sweb/nginx/admin/start.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
+config_nginx(){
+   #nginx -g 'daemon off;'
+   nginx &
+}
+
+#....
+
+load_entrypoint_base(){
+   #ejecutar entrypoint ubbase
+   /root/admin/start.sh
+  
+}
 
 main(){
-   
-   tail -f /dev/null 
+ 
+  config_nginx
+  load_entrypoint_base
+    
 }
 
 main
